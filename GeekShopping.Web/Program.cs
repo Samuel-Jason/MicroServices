6f5263c8a8d71dbbdb9dd7b0176bb33c80174a7d
Samuel <samuelljason@gmail.com>
2024-08-20 15:28:42 -0300

    Alterações na controller e nas configurações

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,7 +1,14 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+    c.BaseAddress = new Uri(configuration["ServiceUrls:ProductAPI"])
+    );
 
 var app = builder.Build();
 
